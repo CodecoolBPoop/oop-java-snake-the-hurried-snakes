506b6f1f0318adf87383ef83744a3a0fe6471509
Halmai Mirella <halmaimirella@gmail.com>
2019-01-03 13:08:15 +0100

    refactor: moving position etc. setting to super

--- src/com/codecool/snake/entities/enemies/Enemy.java
+++ src/com/codecool/snake/entities/enemies/Enemy.java
@@ -1,16 +1,55 @@
 package com.codecool.snake.entities.enemies;
 
+import com.codecool.snake.Globals;
+import com.codecool.snake.Utils;
 import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.entities.snakes.Snake;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.geometry.Point2D;
+
+import java.util.Random;
 
 public abstract class Enemy extends GameEntity{
     private final int damage;
+    private static Random rnd = new Random();
+    private Point2D heading;
 
-    public Enemy(int damage) {
+    public Enemy(int damage, int speed) {
         super();
         this.damage = damage;
+        setPositionAtRandom();
+        double direction = setDirectionAtRandom();
+        setRotate(direction);
+        heading = Utils.directionToVector(direction, speed);
+    }
+
+    public double setDirectionAtRandom() {
+        return rnd.nextDouble() * 360;
+    }
+
+    public void setPositionAtRandom() {
+        double x = rnd.nextDouble() * Globals.WINDOW_WIDTH;
+        setX(x);
+        Snake snake = Globals.getInstance().game.getSnake();
+        while (true) {
+            double y = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
+            setY(y);
+            if (!onSnakeHead(this, snake)) {
+                break;
+            }
+        }
     }
 
     public int getDamage() {
         return damage;
+    }
+
+    public boolean onSnakeHead(Enemy enemy, Snake snake) {
+        SnakeHead head = snake.getHead();
+        return head.getBoundsInParent().intersects(enemy.getBoundsInParent());
+    }
+
+    public Point2D getHeading() {
+        return heading;
     }
 }
--- src/com/codecool/snake/entities/enemies/SimpleEnemy.java
+++ src/com/codecool/snake/entities/enemies/SimpleEnemy.java
@@ -3,47 +3,16 @@ package com.codecool.snake.entities.enemies;
 import com.codecool.snake.entities.GameEntity;
 import com.codecool.snake.Globals;
 import com.codecool.snake.entities.Animatable;
-import com.codecool.snake.Utils;
 import com.codecool.snake.entities.Interactable;
 import com.codecool.snake.entities.snakes.SnakeHead;
-import com.codecool.snake.entities.snakes.Snake;
-import java.util.Random;
-
-import javafx.geometry.Point2D;
 
 
 
 public class SimpleEnemy extends Enemy implements Animatable, Interactable {
 
-    private Point2D heading;
-    private static Random rnd = new Random();
-
     public SimpleEnemy() {
-        super(10);
-
+        super(10, 1);
         setImage(Globals.getInstance().getImage("SimpleEnemy"));
-
-        double x = rnd.nextDouble() * Globals.WINDOW_WIDTH;
-        setX(x);
-        Snake snake = Globals.getInstance().game.getSnake();
-        while (true) {
-            double y = rnd.nextDouble() * Globals.WINDOW_HEIGHT;
-            setY(y);
-            if (!onSnakeHead(this, snake)) {
-                break;
-            }
-        }
-
-        double direction = rnd.nextDouble() * 360;
-        setRotate(direction);
-
-        int speed = 1;
-        heading = Utils.directionToVector(direction, speed);
-    }
-
-    private boolean onSnakeHead(SimpleEnemy enemy, Snake snake) {
-        SnakeHead head = snake.getHead();
-        return head.getBoundsInParent().intersects(enemy.getBoundsInParent());
     }
 
     @Override
@@ -51,8 +20,8 @@ public class SimpleEnemy extends Enemy implements Animatable, Interactable {
         if (isOutOfBounds()) {
             destroy();
         }
-        setX(getX() + heading.getX());
-        setY(getY() + heading.getY());
+        setX(getX() + getHeading().getX());
+        setY(getY() + getHeading().getY());
     }
 
     @Override
